7a8832b29a8796252008c45d58857ee73a248087
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2019-07-31 13:48:01 +0300

    login and register

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const {ensureAuthenticated} = require('../helpers/auth');
+
+
 //Load Idea mongoose
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
 
 //Idea index page
-router.get('/', (req, res) => {
-  Idea.find({})
+router.get('/', ensureAuthenticated,  (req, res) => {
+  Idea.find({user: req.user.id})
   .sort({date:'desc'})
   .then(ideas => {
     res.render('ideas/index', {
@@ -20,22 +23,31 @@ router.get('/', (req, res) => {
 
 
 //Add Idea form
-router.get('/add', (req, res)=>{
+router.get('/add', ensureAuthenticated, (req, res)=>{
   res.render('ideas/add');
 });
 
-router.get('/edit/:id', (req, res)=>{
+//Edit Idea form
+router.get('/edit/:id', ensureAuthenticated, (req, res)=>{
   Idea.findOne({
     _id: req.params.id
   })
   .then(idea => {
+    if(idea.user != req.user.id){
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else {
+      
     res.render('ideas/edit',{
       idea:idea
     });
-  }); 
-});
+  }
 
-router.post('/', (req, res) => {
+    });
+  }); 
+
+//Process Form
+router.post('/', ensureAuthenticated, (req, res) => {
   let errors = [];
 
   if(!req.body.title){
@@ -56,7 +68,8 @@ router.post('/', (req, res) => {
   }else{
     const newUser = {
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     }
     new Idea(newUser)
       .save()
@@ -68,7 +81,7 @@ router.post('/', (req, res) => {
 });
 
 //Edit fórm process
-router.put('/:id', (req, res)=> {
+router.put('/:id', ensureAuthenticated, (req, res)=> {
   Idea.findOne({
     _id: req.params.id
   })
@@ -86,7 +99,7 @@ router.put('/:id', (req, res)=> {
 });
 
 //Delete ideas
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
   Idea.remove({_id: req.params.id})
   .then(()=> {
     req.flash('success_msg', 'Video idea removed')
